4b96fa1d788b780a10da025314f088953ead1654
hour01 <871801447@qq.com>
2025-05-29 16:54:56 +0800

    数据库修改

--- utils/DB.py
+++ utils/DB.py
@@ -6,13 +6,14 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 import copy
-
+import re
+from utils.functions import query_LLM_ollama
 # predefined info
 HOST = '60.204.204.207'
 PORT = 3306
 USER = 'hagongda'
 PASSWD = 'haG[o#8n.g]d68a'
-DB_NAME = 'yf_cats'
+DB_NAME = 'yf_cats_test2'
 
 TEMPLATE = """### Task
 Generate a SQL query to answer [QUESTION]{question}[/QUESTION]
@@ -30,9 +31,29 @@ Given the database schema, here is the SQL query that answers [QUESTION]{questio
 [SQL]
 """
 
+MAX_REVISE_ROUND = 5
+
+REVISE_PROMPT ='''
+以下是一个在MySQL上执行的语句：
+{wrong_sql}
+
+发生报错：{error}
+
+请你修改给出正确的SQL语句，用<SQL>xxx</SQL>形式给出修改后的SQL语句。
+'''.strip()
+
+SELECT_PROMPT="""
+以下是数据库的所有表和字段信息：
+{database_information}
+以下是一个关于该数据库的问题：
+{question}
+请你根据这个问题从以上所有表中选出最相关的15-20个表，并且返回表的英文名，用<RES>table_name1, table_name2, ...</RES> （用逗号分隔每个table_name，整体结果包裹在<RES></RES>之间）的形式给出结果。
+"""
+
+
 class DB:
 
-    def __init__(self, sqlcoder_path = None):
+    def __init__(self, sqlcoder_path = '/data1/public/models/llama-3-sqlcoder-8b/', init_llm=False, selector_path = '/data1/public/models/Qwen2.5-14B-Instruct/'): # L20-old
         # init MySQL
         self.conn = None
         self.cur = None
@@ -55,7 +76,9 @@ class DB:
         self.sqlcoder = None
         self.tokenizer = None
         self.template = TEMPLATE
-        self._init_sqlcoder(sqlcoder_path)
+        if init_llm:
+            self._init_sqlcoder(sqlcoder_path)
+            self._init_selector(selector_path)
         
         
     def _init_mysql(self):
@@ -65,6 +88,16 @@ class DB:
     def _init_sqlcoder(self, path):
         self.tokenizer = AutoTokenizer.from_pretrained(path)
         self.sqlcoder = AutoModelForCausalLM.from_pretrained(
+            path,
+            trust_remote_code=True,
+            torch_dtype=torch.float16,
+            device_map='auto'
+            # device_map={"": int(os.environ.get("LOCAL_RANK") or 0)}
+        )
+        
+    def _init_selector(self, path):
+        self.tokenizer_selector = AutoTokenizer.from_pretrained(path)
+        self.selector = AutoModelForCausalLM.from_pretrained(
             path,
             trust_remote_code=True,
             torch_dtype=torch.float16,
@@ -189,6 +222,55 @@ class DB:
         #     line = f"{table_name}: {table_comment}"
         #     lines.append(line)
         # return "\n\n".join(lines)
+        
+    def select_table(self,question):
+        '''
+        依据question和所有的表的名字和列的介绍，生成候选表
+        输入：
+            question：需要查询的问题
+        返回：
+            selected_table_name：List形式组织的各个表名，[table_name1, table_name2,...]
+        '''
+        
+        query=SELECT_PROMPT.format(database_information=self.get_database_comments(), question=question)
+        
+        prompt = query
+        messages = [
+            {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+        text = self.tokenizer_selector.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+        model_inputs = self.tokenizer_selector([text], return_tensors="pt").to(self.selector.device)
+
+        generated_ids = self.selector.generate(
+            **model_inputs,
+            max_new_tokens=1024
+        )
+        generated_ids = [
+            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+        ]
+
+        response = self.tokenizer_selector.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    
+        print(response)
+        
+        import re
+
+        # 从 outputs 中提取 <RES> 标签之间的内容
+        match = re.search(r"<RES>(.*?)</RES>", response, re.DOTALL)
+        if match:
+            table_str = match.group(1)
+            # 按逗号分隔，去除前后空格
+            selected_table_name = [t.strip() for t in table_str.split(",") if t.strip()]
+        else:
+            selected_table_name = []
+
+        
+        return selected_table_name
     
     def generate_sql(self, selected_table_name, question):
         '''
@@ -219,6 +301,18 @@ class DB:
         comment = sql_output.split('assistant')[1].strip()
         
         return sql_code
+    
+    
+    def revise_sql(self, wrong_sql, res):
+        response = query_LLM_ollama(llm_name='reasoning-LLM', query=REVISE_PROMPT.format(wrong_sql=wrong_sql, error=res['error']))
+        def extract_sql(text):
+            # 使用正则表达式提取 <SQL> 和 </SQL> 之间的内容
+            match = re.search(r"<SQL>(.*?)</SQL>", text, re.DOTALL)
+            if match:
+                return match.group(1).strip()
+            return None
+        new_sql_code = extract_sql(response).strip()
+        return new_sql_code
     
     def execute_sql(self, sql):
         """
@@ -252,3 +346,37 @@ class DB:
             return {
                 "error": str(e)
             }
+
+
+    def query_db(self, text_query):
+        """
+        输入针对数据库的文本形式问题，查询数据库数据，若正确执行，返回结果（SELECT 结果为 dict）：
+            dcit['rowcount']：返回的结果行数
+            dict['fields']：返回结果表的列信息
+            dict['data']：list形式的每一行数据，每一行数据由字典形式组成：[{field1: xxx, field2: xxxx}, ...]
+        
+        若执行出现错误抛出错误信息：
+            raise RuntimeError(f"An error occurred while executing the database query: {e}")
+        """
+        
+        try:
+            # TODO: table_name
+            selected_table_name = self.select_table(question=text_query.strip())
+            sql_code = self.generate_sql(selected_table_name = selected_table_name, question=text_query.strip()).strip()
+            res = self.execute_sql(sql_code)
+            cnt = 0
+            while 'error' in res and cnt < MAX_REVISE_ROUND:
+                sql_code = self.revise_sql(wrong_sql=sql_code, res=res).strip()
+                print(f"Round: {cnt}")
+                res = self.execute_sql(sql_code)
+                print(res)
+                cnt += 1
+            if 'error' in res:
+                # print('Failed!')
+                raise RuntimeError(f"Database query failed after {cnt} attempts. Final SQL: {sql_code}\nError: {res['error']}")
+            else:
+                print(sql_code)
+                return res
+                
+        except Exception as e:
+            raise RuntimeError(f"An error occurred while executing the database query: {e}")
